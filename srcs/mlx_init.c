eeece1de09fd55ada0b5455a8884363a08ba3682
J-spn <johnnywallker@inbox.ru>
2022-09-19 18:20:32 +0300

    Ok Parsing; add wolf_textures

--- srcs/mlx_init.c
+++ srcs/mlx_init.c
@@ -6,7 +6,7 @@ void	ft_mlx(t_data *data)
 	data->mlx = mlx_init();
 	if (data->mlx == (void *)0)
 		ft_exit_error("Error\nmlx_init failed\n");
-	data->mlx_win = mlx_new_window(data->mlx, 1000, 500, PROG_NAME);
+	data->mlx_win = mlx_new_window(data->mlx, 1000, 500, GAME_NAME);
 	if (data->mlx_win == (void *)0)
 		ft_exit_error("Error\nmlx_window failed\n");
 	mlx_hook(data->mlx_win, 17, 0, ft_free_exit, data);
